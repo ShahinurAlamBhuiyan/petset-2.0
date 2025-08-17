5577659f7b91776c327c7c3e74a304b96f059d95
ShahinurAlamBhuiyan <shahinur.alam.bhuiyan01@gmail.com>
2025-08-17 22:26:01 +0600

    implementing navbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import ReduxProvider from "@/providers/ReduxProvider";
 import TopBar from "@/components/shared/TopBar";
+import NavBar from "@/components/shared/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <ReduxProvider>
           <TopBar />
+          <NavBar />
           {children}
         </ReduxProvider>
       </body>

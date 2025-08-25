b8330d94f6b148f49184140af8d97fba8fd184bf
ShahinurAlamBhuiyan <shahinur.alam.bhuiyan01@gmail.com>
2025-08-25 20:45:20 +0600

    rearrange components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,15 @@
 import AboutUs from "@/components/home/AboutUs";
 import HomeCarousel from "@/components/home/HomeCarousel";
 import OurFeatures from "@/components/home/OurFeatures";
+import WhyChoose from "@/components/home/WhyChoose";
 
 export default function Home() {
   return (
     <>
       <HomeCarousel />
-      <AboutUs />
+      <WhyChoose />
       <OurFeatures />
+      <AboutUs />
     </>
   );
 }
